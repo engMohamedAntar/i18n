dca2a51ac5bc6cf479dcb74a0128f9ab9151c4b7
MohamedAntar <aboantar852003@gmail.com>
2026-03-08 23:03:49 +0200

    imported I18Module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongoModule } from './mongo/mongo.module';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, MongoModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    MongoModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-custom-lang']),
+      ],
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
